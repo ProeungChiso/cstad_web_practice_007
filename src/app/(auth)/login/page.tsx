7bde4e70151a55cf7527895e4bd80bd176c06ba7
ProeungChiso <proeung.chiso.2821@rupp.edu.kh>
2024-03-26 16:43:30 +0700

    generated titles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,20 @@
+import { Metadata } from 'next';
 import React from 'react';
-
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Clothes Shop Website.",
+    keywords: ["shop", "ecommerce", "sell"],
+    openGraph: {
+      title: {
+        template: "%s - ACMEShop",
+        default: "ACMEShop",
+      },
+      description: "Clothes Shop Website",
+      images: [
+        "https://img.freepik.com/free-photo/photocomposition-horizontal-shopping-banner-with-online-shop-smartphone_23-2151201769.jpg?t=st=1711445270~exp=1711448870~hmac=a9bdd24fb0e0c9bf61350249a48a4fba5bdac187bf6c2bba8f38a9f481ccf612&w=1480",
+      ],
+    },
+  };
 export default function logIn(){
     return(
         <h1>Login</h1>
